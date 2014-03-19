ab7f81884c1a4db9857de21025b1ca1a6d62612d
Jonathan Toppins <jtoppins@users.sourceforge.net>
2014-03-19 17:05:26 -0400

    Initial commit

---
+++ bin/build-cscope.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+unset TMPDIR
+cwd=$(pwd)
+filename=cscope.files
+
+#find ${cwd} -type f  -name "*.[chxsS]" >-o -name "Makefile" \
+#        -o -name "*.inc" -o -name "*.mk" \
+#        -o -name "*.sh" >cscope.files && cscope -bkq || \
+#	echo "Error: failed to generate cscope database" 1>&2
+
+find ${cwd} -type f  -name "*.[chxsS]"  >${cwd}/${filename} && \
+find ${cwd} -type f  -name "*.cpp"     >>${cwd}/${filename} && \
+find ${cwd} -type f  -name "*.hpp"     >>${cwd}/${filename} && \
+find ${cwd} -type f  -name "*.cc"      >>${cwd}/${filename} && \
+find ${cwd} -type f  -name "Makefile"  >>${cwd}/${filename} && \
+find ${cwd} -type f  -name "*.inc"     >>${cwd}/${filename} && \
+find ${cwd} -type f  -name "*.mk"      >>${cwd}/${filename} && \
+find ${cwd} -type f  -name "*.sh"      >>${cwd}/${filename} && \
+cscope -bkq && rm -f ${cwd}/${filename} || \
+        echo "Error: failed to generate cscope database" 1>&2
